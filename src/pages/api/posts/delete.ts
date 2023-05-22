38aafa3f344086cee81a7ecdb04a5e2bdae65d74
Erhan Gocen <71042486+erhangocen@users.noreply.github.com>
2023-05-22 04:53:33 +0300

    .

---
+++ src/pages/api/posts/delete.ts
@@ -0,0 +1,19 @@
+import { withMethods } from "@/lib/api-middlewares/with-methods";
+import { db } from "@/lib/db";
+import { Prisma } from "@prisma/client";
+import { error } from "console";
+import { NextApiRequest, NextApiResponse } from "next";
+import { type } from "os";
+import { any, z } from "zod";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    const {id} = req.body;
+
+    try{
+        await db.post.delete({where:{id:id}})
+        return res.status(200).json({"success": "true"});
+    }catch (error){
+        return res.status(500).json({error:error})
+    }
+}
